8fbcdd0b2bdb5c29cffde946f11424833d3b506e
Amaury Diaz <amauryuh@gmail.com>
2019-04-17 13:26:09 -0400

    V1.0

---
+++ precache-manifest.126fdda9f25e447e24de0c0ec9efb3ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ca647f01516b89f4ff8a",
+    "url": "/static/js/main.ca647f01.chunk.js"
+  },
+  {
+    "revision": "cf4d4d560311fe77cf96",
+    "url": "/static/js/2.cf4d4d56.chunk.js"
+  },
+  {
+    "revision": "ca647f01516b89f4ff8a",
+    "url": "/static/css/main.6a45386c.chunk.css"
+  },
+  {
+    "revision": "927b427e7ccd9d779ff4b8b1e166700b",
+    "url": "/index.html"
+  }
+];
